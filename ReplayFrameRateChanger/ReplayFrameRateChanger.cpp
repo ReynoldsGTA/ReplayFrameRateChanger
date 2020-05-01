5cc94051948a85d0ab02a08bf813f86c02e4b970
Gerben Hettinga <gerbenhettinga@hotmail.com>
2020-05-01 21:37:10 +0200

    Add project files.

---
+++ ReplayFrameRateChanger/ReplayFrameRateChanger.cpp
@@ -0,0 +1,11 @@
+#include "plugin.h"
+
+using namespace plugin;
+
+class ReplayFrameRateChanger {
+public:
+    ReplayFrameRateChanger() {
+        // Initialise your plugin here
+        
+    }
+} replayFrameRateChanger;
